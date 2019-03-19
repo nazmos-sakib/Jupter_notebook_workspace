4d614a032738ec0ba3d7f6e6b24075ed91888a81
nazmos-sakib <nazmosakib3652@live.com>
2019-03-19 21:33:33 +0600

    6th commit

--- flask_thesis_app/predict.py
+++ flask_thesis_app/predict.py
@@ -3,9 +3,25 @@ from flask import request
 from flask import jsonify
 from flask import render_template,flash,redirect,Response
 
-from preprocess_data import prere
+from preprocess_data import user_data_process
+from model import get_model,NN,NN1
 
 import numpy as np
+import pandas as pd
+
+
+'''
+import tensorflow as tf
+
+session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
+
+from keras import backend as K
+
+sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
+K.set_session(sess)
+'''
+
+
 
 #create instance of the flask
 #"__name__" is the name of the application module
@@ -16,17 +32,37 @@ app = Flask(__name__, static_url_path='')
 #app.debug = True
 #app.run(debug=True)
 
-app.config['ENV'] = 'development'
-app.config['DEBUG'] = True
-app.config['TESTING'] = True
+#app.config['ENV'] = 'development'
+#app.config['DEBUG'] = True
+#app.config['TESTING'] = True
+
+
+#model1 = get_model()
+from keras.models import load_model
+print ('load model...')
+model = load_model("heart_attack_risk_prediction_percent_split.h5",compile= False)
+model._make_predict_function()
+print("* Model loaded")
+
+
+def NN2(data):
+    data = np.expand_dims(data, axis=0)
+    #global model
+    predections = model.predict_classes(data,verbose=0)
+    return predections[0]
+
+
+
+print("------------the begin print----")
 
 @app.route("/") # 
 def running():
-    return app.send_static_file('index.html')
+	print("--in route fun--")
+	return app.send_static_file('index.html')
 
 
 
-@app.route('/hello',methods=["POST","GET"])
+@app.route('/hello',methods=["POST"])
 
 def hello():
 	message = request.get_json(force=True)
@@ -55,21 +91,33 @@ def hello():
 
 	#print(type(asd))
 
-	x = []
+	#x = []
 
-	for key in message.keys():
-		x.append(message[key])
+	#for key in message.keys():
+	#	x.append(message[key])
 
 	#print(x)
 
-	user_data_process(x)
-	prere()
+	data = user_data_process(message)
+	print(data)
+	
+	x = []
+
+	for key in data.keys():
+		x.append(data[key])
+
+	print(x)
+	
+	#predicted_value = []
+	
+	#predicted_value = NN(x)
+	predicted_value = NN2(x)
+	#predicted_value = NN1(x,model1)
 
 	respone = {
-	"gretting" : "hello, "+age+". your gender is : "+gender+". your smoking habit: "+smoking+". HTN: "+HTN+". DPL: "+DPL+". DM: "+DM+". physical_exercise: "+physical_exercise
+	"gretting" : "hello, "+age+". your gender is : "+gender+". your smoking habit: "+smoking+". HTN: "+HTN+". DPL: "+DPL+". DM: "+DM+". physical_exercise: "+physical_exercise,
+	"prediction" : str(predicted_value)
 	}
 
 	return jsonify(respone)
 
-def user_data_process(message):
-	pass
